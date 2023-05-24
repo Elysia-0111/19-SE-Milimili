a3a0760ae5920b8a6e0ca6581d4c8507e798284e
menglu227 <1368051488@qq.com>
2023-05-24 10:36:06 +0800

    加入了个人主页界面

---
+++ 前端/milimili/src/views/HomePage.vue
@@ -0,0 +1,12 @@
+<template>
+    <div v-if="this.$store.state.isLogin==false">
+        <div>尚未登录，请前往登录或注册</div>
+        <router-link to="/login2">登录</router-link> |
+        <router-link to="/login">注册</router-link>
+    </div>
+    <div v-else>
+        <div class="personal homepage">
+            <router-link to="/personal">主页界面</router-link>
+        </div>
+    </div>
+    </template>
